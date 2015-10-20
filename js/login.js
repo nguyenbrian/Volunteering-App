b2a4ba6e7adfd0e1f5cac283fe64fad8829d6b9e
nguyenbrian <nguyen.brian70@gmail.com>
2015-10-20 00:36:03 +0000

    prototype display login info as html

--- js/login.js
+++ js/login.js
@@ -2,11 +2,11 @@ function submitUserpass() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     var userPass = "Your user name is " + username + " and your password is " + password + "." + "\n\nProceed to sign-in?";
-    var proceed = confirm(userPass);
-    if (proceed == true) {
-    document.location.href = "html/home.html";
-    }
+    $("#hiddentext").text(userPass)
+    $("#hiddentext").removeAttr("hidden")
+    $("#hiddentext").addClass("animated FadeIn")
+    $("#title").attr("class", "animated fadeOut");
+    $("#loginbox").attr("class", "animated fadeOut");
     
-    else {
-    }
-};
+    
+}
