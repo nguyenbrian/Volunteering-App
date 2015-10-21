e83672a9c1d33edca8d1e2a093f557a0098bb3dd
nguyenbrian <nguyen.brian70@gmail.com>
2015-10-21 14:41:17 +0000

    completed login page

--- js/login.js
+++ js/login.js
@@ -1,12 +1,20 @@
+$( "#userpass" ).submit(function() {
+  event.preventDefault();
+});
+
 function submitUserpass() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    var userPass = "Your user name is " + username + " and your password is " + password + "." + "\n\nProceed to sign-in?";
-    $("#hiddentext").text(userPass)
-    $("#hiddentext").removeAttr("hidden")
-    $("#hiddentext").addClass("animated FadeIn")
+    var userPass = "Your user name is " + "<strong>" +  username + "</strong>" + " and your password is " + "<strong>" + password + "</strong>" + "." + "</br> </br>" + "Proceed to sign-in?";
+    $("#hiddentext").html(userPass);
+    $("#hiddentext").removeAttr("hidden");
+    $("#submitbutton").attr("value","confirm");
+    $("#hiddentext").addClass("animated FadeIn");
     $("#title").attr("class", "animated fadeOut");
     $("#loginbox").attr("class", "animated fadeOut");
+    $("#submitbutton").click("submit", function() {
+    document.location.href = "html/home.html";
+    } )
     
-    
-}
+
+    }
